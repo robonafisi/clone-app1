06b6ac98ce41c94cae806b5a099e604be1d57319
Mohammad Nafisi <nafisidev@gmail.com>
2023-01-15 22:50:34 +0800

    Adding the bulk of the top section

---
+++ src/components/Row.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Row = ({title, fetchURL}) => {
+  return (
+    <div>
+        <h2 className='text-white font-bold md:text-xl p-4'>
+            {title}
+        </h2>
+    </div>
+  )
+}
+
+export default Row
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Main from '../components/Main'
+import requests from '../Requests'
+import Row from '../components/Row'
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Main />
+        <Row title='Up Coming' fetchURL={requests.requestUpcoming} />
+        <Row title='Popular' fetchURL={requests.requestPopular} />
+        <Row title='Trending' fetchURL={requests.requestTrending} />
+        <Row title='Top Rated' fetchURL={requests.requestTopRated} />
+        <Row title='Horror' fetchURL={requests.requestHorror} />
+    </div>
   )
 }
+
+export default Home
