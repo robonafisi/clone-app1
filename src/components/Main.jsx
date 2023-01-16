e44dae8e47db7d3281c7c2f4b40a400a09ad7992
Mohammad Nafisi <nafisidev@gmail.com>
2023-01-16 23:56:03 +0800

    Added the category movies

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,11 +14,11 @@ const Main = () => {
     },[])
 
     const truncateString = (str, num) => {
-        if(str.length > num){
+        if(str?.length > num){
             return str.slice(0, num) + '...'
         } else {
             return str;
-    }}
+    }} 
 
   return (
     <div className='w-full h-[550px] text-white'>
@@ -27,10 +27,10 @@ const Main = () => {
             <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
             <div className='absolute w-full top-[20%] p-4 md:p-8'>
                 <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
-            <div className='my-4'>
+            {/* <div className='my-4'>
                 <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
                 <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
-            </div>
+            </div> */}
             <p className='text-gray-400 text-sm'>Resealed: {movie?.release_date}</p>
             <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>{truncateString(movie?.overview, 150)}</p>
             </div>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,30 +1,34 @@
 import { React, useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Row = ({title, fetchURL}) => {
+const Row = ({ title, fetchURL }) => {
     const [movies, setMovies] = useState([])
 
     useEffect(()=>{
-        axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=962bfd59b89a9c5dc663e0848697858f&language=en-US&page=1').then((response)=>{
+        axios.get(fetchURL).then((response)=>{
             setMovies(response.data.results)
         })
-    },[fetchURL])
-    
+    }, [fetchURL])
+
 
   return (
     <div>
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
         <div className='relative flex items-center'>
             <div id={'slider'}>
-                {movies.map((item, id) =>{
+                {movies.map((item, id)=>(
                     <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
                         <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}/>
+                        <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 text-white'>
+                             <p>{item?.title}</p>
+                        </div>
                     </div>
-                })}
+                ))}
+                
             </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Row
+export default Row;
