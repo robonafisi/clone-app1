f5213c0cd21619a5cd970f3d16f7853b3197b421
Mohammad Nafisi <nafisidev@gmail.com>
2023-01-17 00:10:06 +0800

    Completed the UI

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,10 @@
 import { React, useState, useEffect } from 'react'
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import axios from 'axios'
 
 const Row = ({ title, fetchURL }) => {
     const [movies, setMovies] = useState([])
+    const [like, setLike] = useState(false)
 
     useEffect(()=>{
         axios.get(fetchURL).then((response)=>{
@@ -18,13 +20,13 @@ const Row = ({ title, fetchURL }) => {
             <div id={'slider'}>
                 {movies.map((item, id)=>(
                     <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                        <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}/>
-                        <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 text-white'>
-                             <p>{item?.title}</p>
+                        <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}/>
+                        <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                             <p className='white-space-normal text-md md:text-lg font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
+                             <p>{like ? <FaHeart className='absolute top-4 left-4 text-gray-300'/> : <FaRegHeart className='absolute top-4 left-4 text-gray-300'/>}</p>
                         </div>
                     </div>
                 ))}
-                
             </div>
         </div>
     </div>
